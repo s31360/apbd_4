849ad7718a37b8a309002f4a2f85f59917066798
s31360 <s31360@pjwstk.edu.pl>
2025-04-06 19:12:51 +0200

    Implemented MaxSalary and MinSalaryDep30 tests

--- Tutorial4Tests/obj/Debug/net8.0/Tutorial4Tests.AssemblyInfo.cs
+++ Tutorial4Tests/obj/Debug/net8.0/Tutorial4Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbffac7f823af003885475a8dbc3f48fb5916e19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d17f46b14cf731eab2326f516e9f5aacc28022")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tutorial4Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
